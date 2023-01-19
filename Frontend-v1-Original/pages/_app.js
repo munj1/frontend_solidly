02752405afbb9438c19726ca4e99b6b71fb06758
torbik <liker1011@gmail.com>
2023-01-19 22:35:19 +0700

    Change title, logo and logo text to Velocimeter

--- Frontend-v1-Original/pages/_app.js
+++ Frontend-v1-Original/pages/_app.js
@@ -81,7 +81,7 @@ export default function MyApp ({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Solidly</title>
+        <title>Velocimeter</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
