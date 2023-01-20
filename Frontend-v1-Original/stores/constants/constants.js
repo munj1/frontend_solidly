d93c85eefbf14b57f1463690e61b73ce855f63fb
torbik <liker1011@gmail.com>
2023-01-20 17:16:55 +0700

    arbiscan

--- Frontend-v1-Original/stores/constants/constants.js
+++ Frontend-v1-Original/stores/constants/constants.js
@@ -1,26 +1,23 @@
-import BigNumber from 'bignumber.js'
-import * as contractsTestnet from './contractsGoerli'
-import * as contracts from './contracts'
-// import * as contracts from './contracts'
-import * as actions from './actions'
+import BigNumber from "bignumber.js";
+import * as contractsTestnet from "./contractsGoerli";
+import * as contracts from "./contracts";
+import * as actions from "./actions";
 
-let isTestnet = process.env.NEXT_PUBLIC_CHAINID == 421613
+let isTestnet = process.env.NEXT_PUBLIC_CHAINID == 421613;
 // change this to make it work with arbitrum & goerli as well
 // URLS
-let scan = 'https://ftmscan.com/'
-let cont = contracts
+let scan = "https://arbiscan.io/";
+let cont = contracts;
 
 if (isTestnet) {
-  // scan = 'https://testnet.ftmscan.com/'
-  scan = 'https://goerli.arbiscan.io/'
-  cont = contractsTestnet
-  //cont = contracts
+  scan = "https://goerli.arbiscan.io/";
+  cont = contractsTestnet;
 }
 
-export const ETHERSCAN_URL = scan
+export const ETHERSCAN_URL = scan;
 
-export const CONTRACTS = cont
-export const ACTIONS = actions
+export const CONTRACTS = cont;
+export const ACTIONS = actions;
 
-export const MAX_UINT256 = new BigNumber(2).pow(256).minus(1).toFixed(0)
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const MAX_UINT256 = new BigNumber(2).pow(256).minus(1).toFixed(0);
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
