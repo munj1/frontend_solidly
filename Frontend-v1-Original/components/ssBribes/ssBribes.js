497c301491de0e457ff870845b9c34c4f1be251f
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-01-23 12:56:21 +1100

    add bribes and add add bribe button to bribes page

--- Frontend-v1-Original/components/ssBribes/ssBribes.js
+++ Frontend-v1-Original/components/ssBribes/ssBribes.js
@@ -1,25 +1,64 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Paper } from '@material-ui/core';
+import React, { useState, useEffect, useCallback } from 'react'
 
-import classes from './ssBribes.module.css';
+import classes from './ssBribes.module.css'
 
 import BribeCard from '../ssBribeCard'
 
 import stores from '../../stores'
-import { ACTIONS } from '../../stores/constants';
+import { ACTIONS } from '../../stores/constants'
+import {
+  Paper,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  TablePagination,
+  Typography,
+  Tooltip,
+  Toolbar,
+  IconButton,
+  TextField,
+  InputAdornment,
+  Popper,
+  Fade,
+  Grid,
+  Switch
+} from '@material-ui/core'
 
-export default function ssBribes() {
+import { useRouter } from 'next/router'
+import BigNumber from 'bignumber.js'
+import FilterListIcon from '@material-ui/icons/FilterList'
+import SearchIcon from '@material-ui/icons/Search'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
 
-  const [, updateState] = useState();
-  const forceUpdate = useCallback(() => updateState({}), []);
+export default function ssBribes () {
+  const [, updateState] = useState()
+  const forceUpdate = useCallback(() => updateState({}), [])
 
   const [pairs, setPairs] = useState([])
+
+  const onCreateBribe = () => {
+    router.push('/bribe/create')
+  }
+
+  const router = useRouter()
 
   useEffect(() => {
     const stableSwapUpdated = () => {
       const pairs = stores.stableSwapStore.getStore('pairs')
-      const pairsWithBribes = pairs.filter((pair) => {
-        return pair && pair.gauge != null && pair.gauge.address && pair.gauge.bribes && pair.gauge.bribes.length > 0
+      const pairsWithBribes = pairs.filter(pair => {
+        return (
+          pair &&
+          pair.gauge != null &&
+          pair.gauge.address &&
+          pair.gauge.bribes &&
+          pair.gauge.bribes.length > 0
+        )
       })
       setPairs(pairsWithBribes)
       forceUpdate()
@@ -27,23 +66,41 @@ export default function ssBribes() {
 
     stableSwapUpdated()
 
-    stores.emitter.on(ACTIONS.UPDATED, stableSwapUpdated);
+    stores.emitter.on(ACTIONS.UPDATED, stableSwapUpdated)
     return () => {
-      stores.emitter.removeListener(ACTIONS.UPDATED, stableSwapUpdated);
-    };
-  }, []);
+      stores.emitter.removeListener(ACTIONS.UPDATED, stableSwapUpdated)
+    }
+  }, [])
 
   return (
-    <div className={ classes.container}>
-      <div className={ classes.bribesContainer}>
-        {
-          (pairs && pairs && pairs.length > 0) && pairs.map((pair) => {
-            return pair.gauge.bribes.map((bribe) => {
-              return (<BribeCard pair={ pair } bribe={ bribe } />)
+    <div className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item lg={2} md={2} sm={12} xs={12}>
+          <Button
+            variant='contained'
+            color='secondary'
+            startIcon={<AddCircleOutlineIcon />}
+            size='large'
+            className={classes.buttonOverride}
+            // color='primary'
+            onClick={onCreateBribe}
+          >
+            <Typography className={classes.actionButtonText}>
+              Add Bribe
+            </Typography>
+          </Button>
+        </Grid>
+      </Grid>
+      <div className={classes.bribesContainer}>
+        {pairs &&
+          pairs &&
+          pairs.length > 0 &&
+          pairs.map(pair => {
+            return pair.gauge.bribes.map(bribe => {
+              return <BribeCard pair={pair} bribe={bribe} />
             })
-          })
-        }
+          })}
       </div>
     </div>
-  );
+  )
 }
