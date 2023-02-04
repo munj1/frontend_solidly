cdea8098d54fdd94cbbfeff2c7c68defeb403fb5
dunks411 <54425677+duncan4123@users.noreply.github.com>
2023-02-05 08:21:21 +1100

    check vercel

--- Frontend-v1-Original/components/ssLiquidityPairs/ssLiquidityPairs.js
+++ Frontend-v1-Original/components/ssLiquidityPairs/ssLiquidityPairs.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { Typography } from "@mui/material";
+import React, { useState, useEffect, useCallback } from 'react'
+import { Typography } from '@mui/material'
 
-import classes from "./ssLiquidityPairs.module.css";
+import classes from './ssLiquidityPairs.module.css'
 
-import PairsTable from "./ssLiquidityPairsTable.js";
+import PairsTable from './ssLiquidityPairsTable.js'
 
-import stores from "../../stores";
-import { ACTIONS } from "../../stores/constants";
+import stores from '../../stores'
+import { ACTIONS } from '../../stores/constants'
 
-export default function ssLiquidityPairs() {
-  const [, updateState] = useState();
-  const forceUpdate = useCallback(() => updateState({}), []);
+export default function ssLiquidityPairs () {
+  const [, updateState] = useState()
+  const forceUpdate = useCallback(() => updateState({}), [])
 
-  const [pairs, setPairs] = useState([]);
-  const [pairBalancesLoading, setPairBalancesLoading] = useState(false);
+  const [pairs, setPairs] = useState([])
+  const [pairBalancesLoading, setPairBalancesLoading] = useState(false)
 
-  console.log("dunks pairs", pairs);
+  console.log('dunks pairs', pairs)
 
   useEffect(() => {
     const stableSwapUpdated = () => {
-      setPairs(stores.stableSwapStore.getStore("pairs"));
-      forceUpdate();
-    };
+      setPairs(stores.stableSwapStore.getStore('pairs'))
+      forceUpdate()
+    }
 
-    setPairs(stores.stableSwapStore.getStore("pairs"));
+    setPairs(stores.stableSwapStore.getStore('pairs'))
 
-    console.log("dunksset", setPairs);
+    console.log('dunksset', setPairs)
 
-    stores.emitter.on(ACTIONS.UPDATED, stableSwapUpdated);
+    stores.emitter.on(ACTIONS.UPDATED, stableSwapUpdated)
     return () => {
-      stores.emitter.removeListener(ACTIONS.UPDATED, stableSwapUpdated);
-    };
-  }, []);
+      stores.emitter.removeListener(ACTIONS.UPDATED, stableSwapUpdated)
+    }
+  }, [])
 
   return (
     <div className={classes.container}>
@@ -39,10 +39,10 @@ export default function ssLiquidityPairs() {
         <Typography variant='h1'>Liquidity Pools</Typography>
         <Typography variant='body2'>
           Pair your tokens to provide liquidity. Stake the LP tokens to earn
-          FLOW.
+          FLOW
         </Typography>
       </div>
       <PairsTable pairs={pairs} />
     </div>
-  );
+  )
 }
